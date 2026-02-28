633a046a798c10fcfb996fc1f3acca41bb0506d5
SS TOP-UP <souravbhuiyan12245@gmail.com>
2026-02-28 16:08:19 +0000

    Admin PWA: installable app with manifest, service worker, icon

---
+++ admin-sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'ss-admin-v2';
+const URLS = ['/sstopup/admin.html', '/sstopup/admin-manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
